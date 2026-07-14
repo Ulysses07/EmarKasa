59e40b75aa7bc0e2fb283857a157041ade0a7e2f
Musa Sevinç <musasevinc.2007@outlook.com>
2026-07-14 17:09:39 +0300

    feat(app): Cari editör komutları (ekle/düzenle)

--- Kasa.App.Core/CarilerViewModel.cs
+++ Kasa.App.Core/CarilerViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Kasa.ApiClient;
 
 namespace Kasa.App.Core;
@@ -20,4 +21,25 @@ public partial class CarilerViewModel : TemelViewModel
     }
 
     public Task YukleAsync() => CalistirAsync(DoldurAsync);
+
+    [ObservableProperty] private bool _editorMu;
+    [ObservableProperty] private int _duzenId;        // 0 = yeni
+    [ObservableProperty] private string _duzenAd = "";
+    [ObservableProperty] private bool _duzenAktif = true;
+
+    [RelayCommand]
+    private void Yeni() { DuzenId = 0; DuzenAd = ""; DuzenAktif = true; }
+
+    [RelayCommand]
+    public void Duzenle(CariDto c) { DuzenId = c.Id; DuzenAd = c.Ad; DuzenAktif = c.Aktif; }
+
+    [RelayCommand]
+    private Task KaydetAsync() => CalistirAsync(async () =>
+    {
+        var g = new CariYaz(DuzenAd, DuzenAktif);
+        if (DuzenId == 0) await _api.CariOlusturAsync(g);
+        else await _api.CariGuncelleAsync(DuzenId, g);
+        Yeni();
+        await DoldurAsync();
+    });
 }
