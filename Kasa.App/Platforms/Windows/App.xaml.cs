6086373f6f6879ab01a4fbb8f17dcc034e173252
Musa Sevinç <musa@royalmezat.com>
2026-07-15 20:17:57 +0300

    feat(app): headless hatırlatma kontrol modu + zamanlanmış görev

--- Kasa.App/Platforms/Windows/App.xaml.cs
+++ Kasa.App/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
+using Kasa.App.Platforms.Windows;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,5 +21,18 @@ public partial class App : MauiWinUIApplication
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	protected override async void OnLaunched(LaunchActivatedEventArgs args)
+	{
+		var cmd = Environment.GetCommandLineArgs();
+		if (cmd.Contains(HatirlatmaKontrol.Arg))
+		{
+			var app = CreateMauiApp();                 // DI konteyneri (pencere açmadan)
+			await HatirlatmaKontrol.CalistirAsync(app.Services);
+			Exit();
+			return;
+		}
+		base.OnLaunched(args);
+	}
 }
 
